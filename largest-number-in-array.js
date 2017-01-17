cbe1189c6bec098a1b3993e7d84cbefa44bfb922
Aspen Hollyer <aspen.dollins@gmail.com>
2017-01-17 07:23:44 -0600

    Add comments for clarity

--- largest-number-in-array.js
+++ largest-number-in-array.js
@@ -1,12 +1,14 @@
 function largestOfFour(arr) {
-  // You can do this!
+
   for (var i = 0; i < arr.length; i++) {
     arr[i] = arr[i].sort(function(a, b) {
-      return a - b;
-    }).slice(arr[i].length - 1);
+      return a - b; // Sort smallest-to-largest integer. Otherwise, sort goes by unicode value
+    }).slice(arr[i].length - 1); // Get last (largest) integer
   }
   
-  return arr.reduce( function(x,y) {return x.concat(y);} );
+  return arr.reduce( function(x,y) {return x.concat(y);} ); // Collapse sub-arrays into single array
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]); //[5,27,39,1001]
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]); //[27,5,39,1001]
+largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]); //[9,35,97,1000000]

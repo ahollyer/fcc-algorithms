d3ba50bec48e19b96ea995ac190eabd394208d39
Aspen Hollyer <aspen.dollins@gmail.com>
2017-05-18 11:12:58 -0500

    Complete exercise

---
+++ dc-exercises/madlib.js
@@ -0,0 +1,9 @@
+// madlib.js
+// Aspen Hollyer
+
+//Write a function which is given a name and subject. It returns a new string:
+//(name)'s favorite subject in school is (subject).
+function madLib(name, subject) {
+  return `${name}'s favorite subject in school is ${subject}.`;
+}
+console.log(madLib('Bob Dylan', 'physics'));

0e7e8dfaa673a4da123b80fe06edefc8e2705cd6
Aspen Hollyer <aspen.dollins@gmail.com>
2017-06-01 17:44:15 -0500

    Refactor triangle algorithm

--- cs-class/triangle.js
+++ cs-class/triangle.js
@@ -2,7 +2,7 @@
 // Aspen Hollyer
 
 // Develop an algorithm to generate the following output for a
-// square of size N, and implement it in JS.
+// triangle of size N, and implement it in JS.
 
 // * * * * * * * * * *
 //   * * * * * * * * *
@@ -15,14 +15,12 @@
 //                 * *
 //                   *
 
-var n = 10;
+const n = 10;
 
-function printSquare(size) {
-  let sizeCountdown = size;
+function printTriangle(size) {
   for(let i = 0; i < size; i++) {
-    console.log(' '.repeat(i) + '*'.repeat(sizeCountdown));
-    sizeCountdown--;
+    console.log(' '.repeat(i) + '*'.repeat(size - i));
   }
 }
 
-printSquare(n);
+printTriangle(n);

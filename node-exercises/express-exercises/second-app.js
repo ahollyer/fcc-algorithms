93303835f0847d5ec2e3895be3d8a4c9cd365d99
Aspen Hollyer <aspen.dollins@gmail.com>
2017-06-06 10:45:48 -0500

    Complete step 2

--- node-exercises/express-exercises/second-app.js
+++ node-exercises/express-exercises/second-app.js
@@ -10,6 +10,20 @@ app.get('/', function(request, response) {
   response.send('Hello, world!');
 });
 
+// 2. Add to your program the following pages:
+// "Meow" at the URL /cats
+app.get('/cats/', function(request, response) {
+  response.send('Meow');
+});
+// "Woof" at the URL /dogs
+app.get('/dogs/', function(request, response) {
+  response.send('Woof');
+});
+// "Living together" at the URL /cats_and_dogs
+app.get('/cats_and_dogs/', function(request, response) {
+  response.send('Living together');
+});
+
 app.listen(8000, function() {
   console.log('Listening on port 8000.');
 });

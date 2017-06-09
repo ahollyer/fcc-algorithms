5a29dee4c2f4aee81a21684c0afaf230d4f348e5
Aspen Hollyer <aspen.dollins@gmail.com>
2017-06-09 09:16:08 -0500

    Build basic login system

--- node-exercises/express-exercises/first-app.js
+++ node-exercises/express-exercises/first-app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const body_parser = require('body-parser');
+
+var promise = require('bluebird');
+var pgp = require('pg-promise')({
+  promiseLib: promise
+});
+var db = pgp({database: 'restaurant'});
 
 app.set('view engine', 'hbs');
+app.use(body_parser.urlencoded({extended: false}));
 app.use('/static', express.static('public'));
+
+app.use(function (request, response, next) {
+  console.log(request.method, request.path);
+  next();
+});
 
 app.get('/', function(request, response) {
   response.send('<h1>Hello world!</h1>');
@@ -37,6 +50,29 @@ app.get('/hello', function(request, response) {
     ],
   };
   response.render('hello.hbs', context);
+});
+
+app.get('/form', function(request, response) {
+  response.render('form.hbs');
+});
+
+app.get('/thank-you', function(request, response) {
+  response.render('thank-you.hbs');
+});
+
+app.post('/submit', function(request, response) {
+  console.log(request.body);
+  response.redirect('/thank-you');
+});
+
+app.get('/search', function(request, response, next) {
+  let term = request.query.searchTerm;
+  let query = "SELECT * FROM restaurant WHERE restaurant.name ILIKE '%$1#%'";
+  db.any(query, term)
+    .then(function(resultsArray) {
+      response.render('search_results.hbs', {results: resultsArray});
+    })
+    .catch(next);
 });
 
 app.listen(8000, function() {
